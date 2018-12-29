8281dc83d159197ee428249b8132558d359cfad6
Nikolaj Nyboe <nikolaj@nyboe.dk>
2018-12-29 11:43:39 +0100

    Fetch functions

---
+++ client/src/helpers/utils.js
@@ -0,0 +1,35 @@
+export const get = async url => {
+  const request = await fetch(url);
+  const response = await request.json();
+  return response;
+};
+
+export const post = async (url, body) => {
+  const request = await fetch(url, {
+    method: 'POST',
+    body: body,
+    headers:{
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
+  const response = await request.json();
+  return response;
+};
+
+export const patch = async (url, body) => {
+  const request = await fetch(url, {
+    method: 'PATCH',
+    body: body,
+    headers:{
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
+  const response = await request.json();
+  return response;
+};
+
+export const remove = async url => {
+  const request = await fetch(url, {method: 'DELETE'});
+  const response = await request.json();
+  return response;
+};
