243c7e08c9815424b74284c71c2866531e708e06
Nikolaj Nyboe <nikolaj@nyboe.dk>
2018-12-29 11:41:46 +0100

    Error handling

---
+++ helpers/errorHandling.js
@@ -0,0 +1,19 @@
+exports.catchErrors = (fn) => {
+  return function(req, res, next) {
+    return fn(req, res, next).catch(next);
+  };
+};
+
+exports.notFound = (req, res, next) => {
+  const err = new Error(`The route: ${req.url} doesn't exsist.`);
+  res.status(404);
+  next(err);
+};
+
+exports.handleErrors = (err, req, res, next) => {
+  res.json({
+    error: true,
+    message: err.message,
+    status: res.statusCode
+  });
+};
